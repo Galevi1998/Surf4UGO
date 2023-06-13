08c76fffc971b52253d13dcab66630b255b0a259
oridaniel10 <orizxzx@gmail.com>
2023-06-13 12:48:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+//חיבורים לכל הסיפריות וכל התוכנות
+const express = require("express");
+const path = require("path");
+const app = express();
+const LogInCollection = require("./mongodb");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+//שמירת נתיבים והודעה
+
+const publicPath = path.join(__dirname, '../public');
+console.log(publicPath);
+
+// EJS
+app.set('view engine', 'ejs');
+
+app.use(express.static(publicPath));
+
+// שהמשתמש מסיים עם סיומת סינאפ אז נציב לו את הקובץ signup.ejs
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+// שהמשתמש מסיים עם סיומת רגילה- דיפולטיבית אז נציב לו את הקובץ login.ejs
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+//קבלת הפקודה אקטשיון מ signup.ejs והתמודדות איתה
+app.post('/signup', async (req, res) => {
+    const data = {
+        name: req.body.name,
+        password: req.body.password
+    };
+
+    const checking = await LogInCollection.find({});
+    for(var i=0 ; i<checking.length ; i++){
+    try {
+        if (checking[i].name === req.body.name) {
+            res.send("user details already exist");
+        } else {
+            await LogInCollection.insertMany([data]);
+        }
+    } catch {
+        res.send("wrong inputs");
+    }
+
+    res.status(201).render("home", {
+        naming: req.body.name
+    });
+}
+});
+
+// קבלת אקטשיון מהלוגין.איגיס והתמודדות איתה
+// בדיקה בבסיס נתונים שלנו האם קיין שם משתמש וסיסמא של אותו משתמש
+app.post('/login', async (req, res) => {
+    var flag =0;
+        const check = await LogInCollection.find({});
+        for(var i=0 ; i<check.length;i++){
+        if (check[i].password === req.body.password && check[i].name === req.body.name) {
+            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` });
+            flag=1;
+            break;
+        }
+    }
+    if(flag!=1){
+        res.send('coshilirabak');
+    }
+});
+
+// הפעלת השרת
+app.listen(3000, () => {
+    console.log('port connected');
+});
---
+++ mongodb.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const uri =
+  "mongodb+srv://ori:ori123@myshop.uhdnmyx.mongodb.net/myshopdatabase?retryWrites=true&w=majority";
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Mongoose connected");
+  })
+  .catch((error) => {
+    console.log("Failed to connect to MongoDB:", error);
+  });
+
+const logInSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const LogInCollection = mongoose.model("accounts", logInSchema);
+
+module.exports = LogInCollection;
